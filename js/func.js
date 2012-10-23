4dc6cd17e3ede7a9fa099cf7d93189ccb7f1ba09
Eon Matrix <matrix_eon@yahoo.com>
2012-10-23 10:39:31 -0500

    Con Ready

--- js/func.js
+++ js/func.js
@@ -1,14 +1,8 @@
 $(document).ready(function(e) {
-	alert ('Hola');
-});
-$(function() {
-
-
-$('.btn').bind('tap',function(event) {
-	     var boton = (($(this).attr('class')).split(' '))[1];
-//    alert (boton);
-	     switch (boton){
-     
+ $(function() {
+  $('.btn').bind('tap',function(event) {
+	var boton = (($(this).attr('class')).split(' '))[1];
+    switch (boton){
 	 case 'b1':
 	  navigator.notification.beep(1);
 	 break;
@@ -25,33 +19,24 @@ $('.btn').bind('tap',function(event) {
   	  navigator.notification.vibrate(300);
 	  navigator.notification.vibrate(1000);	  
 	 break;
+    }
 	 
-	 }
-	 
-	 $('#alert').swiperight(function(){
-	navigator.notification.alert("deslizó a la derecha", function (){}, "Practica 1", "Aceptar");
-	
-});
-
-
-$('#confirm').swipeleft(function(){
-	navigator.notification.confirm("¿Que desea hacer?",	function(boton){
-//		alert (boton);
-		switch (boton)
-		{
-		case 1:
-			  navigator.notification.beep(1);
-		break;
-		case 2:
-			  navigator.notification.vibrate(500);
-		break;
-		}
-		}, "Practica 1", "Beep, Vibrar, Cancelar");
-});
-
+	$('#alert').swiperight(function(){
+	 navigator.notification.alert("deslizó a la derecha", function (){}, "Practica 1", "Aceptar");
     });
 
-
-
-
+    $('#confirm').swipeleft(function(){
+	 navigator.notification.confirm("¿Que desea hacer?", function(boton){
+	  switch (boton){
+	   case 1:
+		navigator.notification.beep(1);
+	   break;
+	   case 2:
+		navigator.notification.vibrate(500);
+	   break;
+	  }
+	 }, "Practica 1", "Beep, Vibrar, Cancelar");
+    });
+   });
+  });
 });
