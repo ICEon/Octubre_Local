95d560dd7417f77e7812ba44a2471077cc6630b2
Eon Matrix <matrix_eon@yahoo.com>
2012-10-22 19:49:32 -0500

    Vibrar

--- js/func.js
+++ js/func.js
@@ -4,7 +4,28 @@ $(function() {
 
 $('.btn').bind('tap',function(event) {
 	     var boton = (($(this).attr('class')).split(' '))[1];
-    alert (boton);
+//    alert (boton);
+	     switch (boton){
+     
+	 case 'b1':
+	  navigator.notification.beep(1);
+	 break;
+	 case 'b2':
+	  navigator.notification.beep(2);
+	 break;
+	 case 'b3':
+	  navigator.notification.beep(5);
+	 break;
+	 case 'v1':
+	  navigator.notification.vibrate(500);
+	 break;
+	 case 'v2':
+  	  navigator.notification.vibrate(300);
+	  navigator.notification.vibrate(1000);	  
+	 break;
+	 
+	 }
+
     });
 
 
