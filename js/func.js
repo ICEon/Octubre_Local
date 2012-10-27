041e854a41ae79166aa5194b61d5cd72d6092893
Eon Matrix <matrix_eon@yahoo.com>
2012-10-27 10:08:20 -0500

    N3

--- js/func.js
+++ js/func.js
@@ -1,11 +1,20 @@
 // JavaScript Document
-//para que se ejecuten necesitamos compilar
+// Alerta error
+function alertDismissed() {
+// acción, si fracasa
+}
+//función para mostrar alerta
+function showAlert() {
+navigator.notification.alert(
+    'Deslizó a la derecha',  // mensaje
+    alertDismissed,         // callback
+    'Práctica 1',            // título
+    'Aceptar'                  // nombre del botón
+);
+}
 $(document).ready(function(e) {
-	/*
-	$('.btn').tap(
-	function(){						
-		var boton = (($(this).attr('class')).split(' '))[1];			
-		//alert(boton);	
+	/*$('.btn').tap(function(){
+		var boton = (($(this).attr('class')).split(' '))[1];
 		switch(boton){
 			case 'b1':
 				alert('beep 1');
@@ -13,7 +22,7 @@ $(document).ready(function(e) {
 			case 'b2':
 				alert('beep 2');
 				break;
-			case 'b5':
+			case 'b3':
 				alert('beep 5');
 				break;
 			case 'v1':
@@ -21,63 +30,55 @@ $(document).ready(function(e) {
 				break;
 			case 'v2':
 				alert('vibrar');
-				alert('vibrar');				
+				alert('vibrar');
 				break;
-		}						
-    }
-	);
-	//$('#alert input[type=range]').swiperight(function(){
-	$('#alert').swiperight(function(){
-		alert('Deslizó a la derecha');	
 		}
-	);
-	$('#alert').swipeleft(function(){
-		confirm('¿que desea hacer?');	
-		}
-	);
-	*/
+	});
+	$('#right').swiperight(function(){
+		alert('deslizó a la derecha');
+	});
+	$('#left').swipeleft(function(){
+		confirm('¿Qué desea hacer?');
+	});*/
 	document.addEventListener("deviceready",function(){
-		$('btn').tap(function(){
+        $('.btn').tap(function(){
 			var boton = (($(this).attr('class')).split(' '))[1];
 			switch(boton){
-			case 'b1':
-				navigator.notification.beep(1);
-				break;
-			case 'b2':
-				navigator.notification.beep(2);
-				break;
-			case 'b5':
-				navigator.notification.beep(5);
-				break;
-			case 'v1':
-				navigator.notification.vibrate(500);
-				break;
-			case 'v2':
-				navigator.notification.vibrate(300);
-				navigator.notification.vibrate(1000);				
-				break;
-			}
-		});		
-	
-	$('#right').swiperight(function(){
-		navigator.notification.alert("Deslizó a la derecha", function(){ },"Practica 1","Aceptar");
-		}
-	);
-	$('#left').swipeleft(function(){
-		navigator.notification.confirm('¿que desea hacer?', function(boton){ 
-			switch(boton){
-				case 1:
+				case 'b1':
 					navigator.notification.beep(1);
 					break;
-				case 2:
+				case 'b2':
+					navigator.notification.beep(2);
+					break;
+				case 'b3':
+					navigator.notification.beep(5);
+					break;
+				case 'v1':
 					navigator.notification.vibrate(500);
 					break;
+				case 'v2':
+					navigator.notification.vibrate(300);
+					navigator.notification.vibrate(1000);
+					break;
+			}
+		});
+		$('#right').swiperight(function(){
+			showAlert();
+		});
+		$('#left').swipeleft(function(){
+			navigator.notification.confirm('¿Qué desea hacer?',function(boton){
+				switch(boton){
+					case '0':
+						navigator.notification.beep(1);
+						break;
+					case '1':
+						navigator.notification.vibrate(500);
+						break;	
 				}
-		},"Práctica 1","Beep, Vibrar, Cancelar");
-		}
-	);
-	
-	},false);
-	//navigator.notification.beep(1);	
-	//navigator.notification.alert("Deslizó a la derecha",funtion(){},"Practica 1","Aceptar");
-});
+			},"Práctica 1","Beep,Vibrar,Cancelar");
+		});
+	//navigator.notification.beep(1);
+	//navigator.notification.vibrate(300);
+	//navicator.notification.alert("Desliz&oacute; a la derecha", function(){  }, "Pr&acute;ctica 1", "Aceptar");
+	}, false);
+});// JavaScript Document
