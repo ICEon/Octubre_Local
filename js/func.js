565d34c0f8ac63217b0f014d4d4acfee4bc2539b
ICEon <matrix_eon@yahoo.com>
2013-03-20 09:22:11 -0600

    Update func.js

--- js/func.js
+++ js/func.js
@@ -1,29 +1,26 @@
 $(document).ready(function(e) {
 
- document.addEventListener("deviceready", function() {
-	 $('.btn').tap(function() {
-		 var boton = (($(this).attr('class')).split(' '))[1];                 
-		 switch(boton)
-		{
-		alert (boton);	
+	document.addEventListener("deviceready",function(){
+		$('.btn').tap(function(){
+			var boton = (($(this).attr('class')).split(' '))[1];
+			switch(boton){
+
 			case 'b1':
 				navigator.notification.beep(1);
-			break;
+				break;
 			case 'b2':
 				navigator.notification.beep(2);
-			break;
-			case 'b3':
-				navigator.notification.beep(3);
-			break;
+				break;			
 			case 'v1':
 				navigator.notification.vibrate(500);
-			break;
+				break;
 			case 'v2':
-				navigator.notification.vibrate(500);
-				navigator.notification.vibrate(1000);
-			break;
-		}
-	 });
- },false);	
+				navigator.notification.vibrate(300);
+				navigator.notification.vibrate(1000);				
+				break;
+			}
+		});
+	
+	},false);
 
 });
